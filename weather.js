07a1e145d6929706303960e4f7d627dd69e45cf9
Morondo <stepichev2205@gmail.com>
2022-01-12 19:35:15 +0300

    chalk + dedent

--- weather.js
+++ weather.js
@@ -1,11 +1,11 @@
 #!/ust/bin/env node
 import { getArgs } from "./helpers/args.js";
+import { printHelp } from "./services/log.service.js";
 
 const initCLI = () => {
     const args = getArgs(process.argv);
-    console.log(args);
     if (args.h) {
-        // help output
+        printHelp();
     }
     if (args.s) {
         // save the town
