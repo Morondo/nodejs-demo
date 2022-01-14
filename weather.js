98d22b5b11649932e331c5e26d5f989fd6dc7a7b
Morondo <stepichev2205@gmail.com>
2022-01-14 19:14:36 +0300

    work with api

--- weather.js
+++ weather.js
@@ -1,11 +1,19 @@
 #!/ust/bin/env node
 import { getArgs } from "./helpers/args.js";
+import { getWeather } from "./services/api.service.js";
 import { printHelp, printSuccess, printError } from "./services/log.service.js";
-import { saveKeyValue } from "./services/storage.service.js";
+import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Token not received')
+        return;
+    }
     try {
-        await saveKeyValue('token', token);
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
         printSuccess('Token saved');
     } catch(e) {
         printError(e.message);
@@ -14,6 +22,7 @@ const saveToken = async (token) => {
 
 const initCLI = () => {
     const args = getArgs(process.argv);
+    console.log(process.env);
     if (args.h) {
         printHelp();
     }
@@ -23,7 +32,7 @@ const initCLI = () => {
     if (args.t) {
         return saveToken(args.t);
     }
-    // 
+    //getWeather('moscow');
 };
 
 initCLI();
